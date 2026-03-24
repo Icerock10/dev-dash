7e1b33c58c4316512ce42f5d706db6e5fb3d9e2e
Maksym Doroshenko <dp021190dms@gmail.com>
2026-03-24 11:22:21 +0100

    chore: + [nextauth] to ls-lint ignore, * exports in route, expanded User model dd-0

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 import NextAuth from 'next-auth';
-import { authOptions } from '~/features/auth/auth.config';
+import { authOptions } from '~/features/auth/auth';
 import { type NextRequest } from 'next/server';
 
 type AppRouteHandler = (
