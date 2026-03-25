73cf8a7ad280eca7c1bf124408709502e1d8b7f9
Maksym Doroshenko <dp021190dms@gmail.com>
2026-03-25 11:11:53 +0100

    chore: + react-hook-form custom hook dd-0

---
+++ src/hooks/hooks.ts
@@ -0,0 +1,10 @@
+export { useAppForm } from './use-app-form.hook';
+export {
+    useState,
+    useMemo,
+    useEffect,
+    useCallback,
+    useRef,
+    useId,
+} from 'react';
+export { useController as useFormController } from 'react-hook-form';
---
+++ src/hooks/use-app-form.hook.ts
@@ -0,0 +1,91 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { type ZodType as ValidationSchema } from 'zod';
+import {
+    type Control,
+    type DefaultValues,
+    type FieldErrors,
+    type FieldValues,
+    type FormState,
+    type Resolver,
+    useForm,
+    type UseFormGetValues,
+    type UseFormHandleSubmit,
+    type UseFormProps,
+    type UseFormReset,
+    type UseFormSetError,
+    type UseFormSetValue,
+    type UseFormSetFocus,
+    type UseFormWatch,
+    type ValidationMode,
+} from 'react-hook-form';
+
+type Parameters<T extends FieldValues = FieldValues> = {
+    defaultValues: DefaultValues<T>;
+    mode?: keyof ValidationMode;
+    validationSchema?: ZodValidationSchema;
+};
+
+type ReturnValue<T extends FieldValues = FieldValues> = {
+    control: Control<T, null>;
+    dirtyFields?: FormState<T>['dirtyFields'];
+    errors: FieldErrors<T>;
+    getValues: UseFormGetValues<T>;
+    handleSubmit: UseFormHandleSubmit<T>;
+    isDirty: boolean;
+    isSubmitting: boolean;
+    reset: UseFormReset<T>;
+    setError: UseFormSetError<T>;
+    setValue: UseFormSetValue<T>;
+    setFocus: UseFormSetFocus<T>;
+    watch: UseFormWatch<T>;
+};
+
+type ZodValidationSchema<T extends FieldValues = FieldValues> =
+    ValidationSchema<T, T>;
+
+const useAppForm = <T extends FieldValues = FieldValues>({
+    defaultValues,
+    mode = 'onSubmit',
+    validationSchema,
+}: Parameters<T>): ReturnValue<T> => {
+    let parameters: UseFormProps<T> = {
+        defaultValues,
+        mode,
+    };
+
+    if (validationSchema) {
+        parameters = {
+            ...parameters,
+            resolver: zodResolver(validationSchema) as Resolver<T>,
+        };
+    }
+
+    const {
+        control,
+        formState: { dirtyFields, errors, isDirty, isSubmitting },
+        getValues,
+        handleSubmit,
+        reset,
+        setError,
+        setValue,
+        watch,
+        setFocus,
+    } = useForm<T>(parameters);
+
+    return {
+        control,
+        dirtyFields,
+        errors,
+        getValues,
+        handleSubmit,
+        isDirty,
+        isSubmitting,
+        reset,
+        setError,
+        setValue,
+        watch,
+        setFocus,
+    };
+};
+
+export { useAppForm };
