70f287dbccb2c05d97b9a9a396f98330facf5522
Maksym Doroshenko <dp021190dms@gmail.com>
2026-03-27 17:40:17 +0100

    refactor: * separate error handler dd-4

--- src/shared/libs/modules/route-handler/base-route-handler.module.ts
+++ src/shared/libs/modules/route-handler/base-route-handler.module.ts
@@ -1,11 +1,8 @@
 import { NextResponse, type NextRequest } from 'next/server';
+import { handleRouteError } from './libs/helpers/helpers';
 import { type ValueOf } from '~/shared/libs/types/types';
 import { type ZodType } from 'zod';
-import {
-    HTTPError,
-    ValidationError,
-} from '~/shared/libs/modules/exceptions/exceptions';
-import { HTTPCode, DefaultErrorMessage } from '~/shared/libs/enums/enums';
+import { type HTTPCode } from '~/shared/libs/enums/enums';
 
 type Constructor<TBody, TResult> = {
     schema: ZodType<TBody>;
@@ -35,34 +32,7 @@ class RouteHandler<TBody, TResult> {
             const result = await this.handler(body, req);
             return NextResponse.json(result, { status: this.status });
         } catch (error) {
-            if (error instanceof ValidationError) {
-                return NextResponse.json(
-                    {
-                        errors: error.issues.map((issue) => ({
-                            field: issue.path.join('.'),
-                            message: issue.message,
-                        })),
-                    },
-                    { status: HTTPCode.BAD_REQUEST },
-                );
-            }
-            if (error instanceof HTTPError) {
-                return NextResponse.json(
-                    {
-                        message: error.message,
-                        status: error.status,
-                        name: error.name,
-                    },
-                    { status: error.status },
-                );
-            }
-            return NextResponse.json(
-                {
-                    message: DefaultErrorMessage.INTERNAL_ERROR,
-                    status: HTTPCode.INTERNAL_SERVER_ERROR,
-                },
-                { status: HTTPCode.INTERNAL_SERVER_ERROR },
-            );
+            return handleRouteError(error);
         }
     };
 }
