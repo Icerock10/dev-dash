ce868eea56879d8fe511653fd9cfaa242e5c871e
Maksym Doroshenko <dp021190dms@gmail.com>
2026-03-27 10:19:18 +0100

    feat: + tsx dep to run seeds, * scripts in package, updated prisma config dd-2

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     schema: 'src/shared/libs/modules/prisma/schema/',
     migrations: {
         path: 'src/shared/libs/modules/prisma/migrations',
+        seed: 'tsx src/shared/libs/modules/prisma/seeds/seed.ts',
     },
     datasource: {
         url: process.env['DATABASE_URL'] as string,
