711a7d0aa59bb1921fae8b41d5de4a20589c4f70
Maksym Doroshenko <dp021190dms@gmail.com>
2026-03-30 19:59:01 +0200

    refactor: + job-list-client to handle cards state dd-4

---
+++ src/widgets/jobs-list/ui/jobs-list-client.tsx
@@ -0,0 +1,27 @@
+'use client';
+import { useCallback, useState } from '~/shared/hooks/hooks';
+import { type JobDto } from '~/entities/job/index';
+import { JobCard } from '~/entities/job/ui/job-card';
+
+const JobsListClient: React.FC<{ jobs: JobDto[] }> = ({ jobs }) => {
+    const [openJobId, setOpenJobId] = useState<string | null>(null);
+
+    const onOpen = useCallback((payload: null | string) => {
+        setOpenJobId(payload);
+    }, []);
+
+    return (
+        <div className="xs:grid-cols-1 grid gap-3 p-6 md:grid-cols-2 lg:grid-cols-3">
+            {jobs.map((job) => (
+                <JobCard
+                    key={job.id}
+                    job={job}
+                    openJobId={openJobId}
+                    onOpen={onOpen}
+                />
+            ))}
+        </div>
+    );
+};
+
+export { JobsListClient };
--- src/widgets/jobs-list/ui/jobs-list.tsx
+++ src/widgets/jobs-list/ui/jobs-list.tsx
@@ -1,15 +1,9 @@
-import { JobCard } from '~/entities/job/ui/job-card';
 import { jobService } from '~/entities/job/index';
+import { JobsListClient } from './jobs-list-client';
 
 const JobsList: React.FC = async () => {
     const jobs = await jobService.getAll();
-    return (
-        <div className="xs:grid-cols-1 grid gap-3 p-6 md:grid-cols-2 lg:grid-cols-3">
-            {jobs.map((job) => (
-                <JobCard key={job.id} job={job} />
-            ))}
-        </div>
-    );
+    return <JobsListClient jobs={jobs} />;
 };
 
 export { JobsList };
