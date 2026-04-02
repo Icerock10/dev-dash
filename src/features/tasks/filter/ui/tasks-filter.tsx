d91bdac2fe6d1f3da93c66f7ebb8493558d60fe5
Maksym Doroshenko <dp021190dms@gmail.com>
2026-04-02 10:23:56 +0200

    feat: add task base layout dd-11

---
+++ src/features/tasks/filter/ui/tasks-filter.tsx
@@ -0,0 +1,9 @@
+const TasksFilter: React.FC = () => {
+    return (
+        <div className="flex items-center gap-6 border-b border-white/6 px-6 py-3">
+            Filters
+        </div>
+    );
+};
+
+export { TasksFilter };
