6f1297bd4802b237be4bf0ec2b729377f0833018
Maksym Doroshenko <dp021190dms@gmail.com>
2026-04-04 14:17:53 +0200

    feat: + stats overview page dd-17

--- src/widgets/overview/ui/overview-page.tsx
+++ src/widgets/overview/ui/overview-page.tsx
@@ -1,11 +1,14 @@
 import { Header } from '../../header/index';
 import { formatFullDate } from '~/shared/libs/helpers/helpers';
+import { StatsOverview } from './stats-overview';
+import { type JobWithTasksDto } from '~/entities/task/index';
 
 type Properties = {
     userName: string;
+    jobs: JobWithTasksDto[];
 };
 
-const OverviewPage: React.FC<Properties> = ({ userName }) => {
+const OverviewPage: React.FC<Properties> = ({ userName, jobs }) => {
     const today = formatFullDate(new Date());
 
     return (
@@ -13,6 +16,9 @@ const OverviewPage: React.FC<Properties> = ({ userName }) => {
             <Header title={`Good morning, ${userName} 👋`} subTitle={today}>
                 <></>
             </Header>
+            <div className="m-auto flex max-w-4xl flex-col gap-5 p-6">
+                <StatsOverview jobs={jobs} />
+            </div>
         </div>
     );
 };
